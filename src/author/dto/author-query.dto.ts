a00a010bcba453b9934495b007a63ac13e79178c
maxwellsoftware <aralonwarcraft@gmail.com>
2024-11-05 01:01:08 +0300

    feat(author): add OpenAPI types for findAll endpoint

--- src/author/dto/author-query.dto.ts
+++ src/author/dto/author-query.dto.ts
@@ -1,14 +1,24 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { PaginationMetaResponseDto, PaginationRequestDto } from 'src/common/dto/pagination.dto';
 import { SortOrder } from 'src/common/enums/query.enum';
 import { AuthorWithCompositionsDto } from './author.dto';
 
 export class AuthorQueryDto extends PaginationRequestDto {
+  @ApiPropertyOptional({
+    description: 'Author name',
+    type: 'string',
+  })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiPropertyOptional({
+    description: 'Order by ',
+    enum: () => SortOrder,
+    enumName: 'SortOrder',
+    example: SortOrder.ASC,
+  })
   @IsOptional()
   @IsEnum(SortOrder)
   orderBy?: SortOrder = SortOrder.ASC;
